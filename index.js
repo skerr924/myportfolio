0c957f0ce94b585f5f6115955c77061539bfaf10
Sarah Kerr <sarah.kerr93@gmail.com>
2020-08-30 12:59:47 -0400

    remove console.log

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+
+
 document.addEventListener('DOMContentLoaded', function() {
-    console.log("fartypants");
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, {});
   });
