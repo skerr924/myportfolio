941fcb530953be72910c7b1451525afe76ee47a3
Sarah Kerr <sarah.kerr93@gmail.com>
2020-08-30 12:59:02 -0400

    mobile responsive header

---
+++ index.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("fartypants");
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, {});
+  });
