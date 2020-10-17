37bbe05d2431e8d6d729d0d19bdd342fd2ed4f2c
Sarah Kerr <sarah.kerr93@gmail.com>
2020-10-17 13:43:46 -0400

    deploy and predeploy scripts

---
+++ build/precache-manifest.3b913cb8d04e2ba140d3ca2292d7e050.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20f2d184d04c419d210a08502afca2c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9c00c754bb0208835f8",
+    "url": "/static/css/main.4d838493.chunk.css"
+  },
+  {
+    "revision": "3bb7cb1b331cdb727475",
+    "url": "/static/js/2.11eac1fb.chunk.js"
+  },
+  {
+    "revision": "f3df11ca4cc8eaf0f270098bfa135497",
+    "url": "/static/js/2.11eac1fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9c00c754bb0208835f8",
+    "url": "/static/js/main.f986ad54.chunk.js"
+  },
+  {
+    "revision": "056b43112fabbebc80f2",
+    "url": "/static/js/runtime-main.3d086d1e.js"
+  },
+  {
+    "revision": "744d76232a986d312a2a83802e7b9805",
+    "url": "/static/media/SarahKerr.744d7623.jpg"
+  }
+]);
